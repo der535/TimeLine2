1cb42a3709e292e352fb87173a4e55363d4aa1a4
OruSL <bototoros@gmail.com>
2026-01-18 20:06:02 +0300

    Документация

--- Assets/Scripts/General/FontController/FontStorage.cs
+++ Assets/Scripts/General/FontController/FontStorage.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 
 namespace TimeLine.General.Installers
 {
-    /// <summary>
+  
     /// Перечисление доступных типов шрифтов в проекте.
     /// Используется для исключения ошибок при поиске шрифта по строковому имени.
-    /// </summary>
+  
     public enum FontNames
     {
         Standard,  // Основной текст интерфейса
@@ -16,19 +16,19 @@ namespace TimeLine.General.Installers
         Arrows     // Специальный шрифт с символами стрелок
     }
 
-    /// <summary>
+    
     /// Хранилище шрифтов (Font Storage).
     /// Позволяет централизованно управлять шрифтами TextMeshPro и получать их по названию.
-    /// </summary>
+
     public class FontStorage : MonoBehaviour
     {
         [Header("Настройки шрифтов")]
         [Tooltip("Список сопоставления логического имени и физического ассета шрифта")]
         [SerializeField] private List<FontItem> fontItems = new();
 
-        /// <summary>
+       
         /// Поиск и получение ассета шрифта по его имени в перечислении.
-        /// </summary>
+      
         /// <param name="fontName">Имя шрифта из списка FontNames</param>
         /// <returns>Ассет TMP_FontAsset или null, если шрифт не найден</returns>
         public TMP_FontAsset GetFont(FontNames fontName)
@@ -46,10 +46,10 @@ namespace TimeLine.General.Installers
         }
     }
 
-    /// <summary>
+ 
     /// Вспомогательный класс для связи названия шрифта с файлом ассета.
     /// Отображается в инспекторе Unity благодаря атрибуту [Serializable].
-    /// </summary>
+    
     [Serializable]
     public class FontItem
     {
